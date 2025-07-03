f8c04830e7933d151b40153a3254891f897b4e96
Daniel <mokapoop24@gmail.com>
2025-07-03 03:56:05 +0300

    upto date - tested with real brainf#ck code. works successfuly

--- Program.cs
+++ Program.cs
@@ -12,19 +12,38 @@ namespace Brainf_ck_Compiler_Engine
         {
             string brainCode = "dbfskdhzggvlfbliff2983876347```+++--->>><<<,,,...-+-+-";
             int testNum = 1;
-            Console.WriteLine($"test {testNum}:\n" + "code " + brainCode + "\n" + BrainFckCompiler.ParseBrainFckCodeToString(brainCode));
+            Console.WriteLine($"test {testNum}:\n" + "code ->\n" + brainCode + "\n" + BrainFckCompiler.ParseBrainFckCodeToString(brainCode));
             Console.WriteLine("\n");
 
             brainCode = "+++ppp+++ppp+++nnn----ppp+++nnnjhshd+hhh++"; testNum++;
-            Console.WriteLine($"test {testNum}:\n" + "code " + brainCode + "\n" + BrainFckCompiler.ParseBrainFckCodeToString(brainCode));
+            Console.WriteLine($"test {testNum}:\n" + "code ->\n" + brainCode + "\n" + BrainFckCompiler.ParseBrainFckCodeToString(brainCode));
             Console.WriteLine("\n");
 
             brainCode = "[+++]"; testNum++;
-            Console.WriteLine($"test {testNum}:\n" + "code " + brainCode + "\n" + BrainFckCompiler.ParseBrainFckCodeToString(brainCode));
+            Console.WriteLine($"test {testNum}:\n" + "code ->\n" + brainCode + "\n" + BrainFckCompiler.ParseBrainFckCodeToString(brainCode));
             Console.WriteLine("\n");
 
             brainCode = "+++ppp[+++[---]---]ppp+++"; testNum++;
-            Console.WriteLine($"test {testNum}:\n" + "code " + brainCode + "\n" + BrainFckCompiler.ParseBrainFckCodeToString(brainCode));
+            Console.WriteLine($"test {testNum}:\n" + "code ->\n" + brainCode + "\n" + BrainFckCompiler.ParseBrainFckCodeToString(brainCode));
+            Console.WriteLine("\n");
+
+            brainCode =
+                "[\n" +
+                "this is an actual brainfuck script for printing hello world.\n" +
+                "]\n" +
+                "++++++++++[>+++++++<-]>++. this prints the letter H\n" +
+                "<++++++++++[>+++<-]>-. this prints the letter e\n" +
+                "+++++++<++[>.<-] this prints \"ll\"\n" +
+                ">+++. this prints the letter o\n" +
+                ">>+++[<+++++++++++>-]<-. this prints a space character\n" +
+                "++++++++>++++[<+++++++++++>-]<+++. this prints the letter W\n" +
+                "<. this prints the letter o\n" +
+                "+++. this prints the letter r\n" +
+                "------. this prints the letter l\n" +
+                "--------. this prints the letter d\n" +
+                "[-]+++[<+++++++++++>-]<. prints the character !\n";
+            testNum++;
+            Console.WriteLine($"test {testNum}:\n" + "code ->\n" + brainCode + "\n" + BrainFckCompiler.ParseBrainFckCodeToString(brainCode));
             Console.WriteLine("\n");
         }
     }
